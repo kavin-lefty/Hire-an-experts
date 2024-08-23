eeac7d2ccbce76704541583f84473018f080523e
Yugandhar-wgs <yugandhar@whyglobalservices.com>
2024-08-23 12:10:12 +0530

    initail commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import { SmoothScrollHero } from './components/image.jsx'
+import ExampleWrapper from './components/simple.jsx'
+
+function App() {
+
+  return (
+    <>
+    {/* <ExampleWrapper/> */}
+    <SmoothScrollHero/>
+    </>
+  )
+}
+
+export default App
